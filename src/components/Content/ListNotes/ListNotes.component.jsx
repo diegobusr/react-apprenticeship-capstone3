7352a66c2ed64dae12d503ed0bbec741ed6fa839
Diego Bustamante <diego.bustamante@wizeline.com>
2022-02-01 15:14:38 -0700

    files: added new file and fixed issue on listNotes

--- src/components/Content/ListNotes/ListNotes.component.jsx
+++ src/components/Content/ListNotes/ListNotes.component.jsx
@@ -1,21 +1,9 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Note from './Note';
 import EmptyListNotes from './EmptyListNotes';
-import { GlobalContext } from '../../App/App';
 import { ListDiv, NoNotesDiv } from './ListNotes.styles';
 
 const ListNotes = ({ notes }) => {
-  const { typeOfNotes } = useContext(GlobalContext).appContext;
-  const isArchivedPage = Boolean(typeOfNotes === 'archived');
-
-  if (notes.length === 0 && isArchivedPage) {
-    return (
-      <NoNotesDiv>
-        <EmptyListNotes />
-      </NoNotesDiv>
-    );
-  }
-
   if (notes.length === 0) {
     return (
       <NoNotesDiv>
